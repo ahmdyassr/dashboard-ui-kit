c17f27d9c81d3c46bd8125dbe4b11f9d5366a6e6
Ahmed Yasser <ahmdyassr@gmail.com>
2017-11-12 04:13:04 +0200

    [refs #0031] Add rounded bars to chartjs

--- src/js/chart-custom.js
+++ src/js/chart-custom.js
@@ -5,6 +5,113 @@
 // Read More: http://www.chartjs.org/docs/latest/
 //
 
+
+
+// draws a rectangle with a rounded top
+Chart.helpers.drawRoundedTopRectangle = function(ctx, x, y, width, height, radius) {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  // top right corner
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  // bottom right   corner
+  ctx.lineTo(x + width, y + height);
+  // bottom left corner
+  ctx.lineTo(x, y + height);
+  // top left   
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+};
+
+Chart.elements.RoundedTopRectangle = Chart.elements.Rectangle.extend({
+  draw: function() {
+    var ctx = this._chart.ctx;
+    var vm = this._view;
+    var left, right, top, bottom, signX, signY, borderSkipped;
+    var borderWidth = vm.borderWidth;
+
+    if (!vm.horizontal) {
+      // bar
+      left = vm.x - vm.width / 2;
+      right = vm.x + vm.width / 2;
+      top = vm.y;
+      bottom = vm.base;
+      signX = 1;
+      signY = bottom > top? 1: -1;
+      borderSkipped = vm.borderSkipped || 'bottom';
+    } else {
+      // horizontal bar
+      left = vm.base;
+      right = vm.x;
+      top = vm.y - vm.height / 2;
+      bottom = vm.y + vm.height / 2;
+      signX = right > left? 1: -1;
+      signY = 1;
+      borderSkipped = vm.borderSkipped || 'left';
+    }
+
+    // Canvas doesn't allow us to stroke inside the width so we can
+    // adjust the sizes to fit if we're setting a stroke on the line
+    if (borderWidth) {
+      // borderWidth shold be less than bar width and bar height.
+      var barSize = Math.min(Math.abs(left - right), Math.abs(top - bottom));
+      borderWidth = borderWidth > barSize? barSize: borderWidth;
+      var halfStroke = borderWidth / 2;
+      // Adjust borderWidth when bar top position is near vm.base(zero).
+      var borderLeft = left + (borderSkipped !== 'left'? halfStroke * signX: 0);
+      var borderRight = right + (borderSkipped !== 'right'? -halfStroke * signX: 0);
+      var borderTop = top + (borderSkipped !== 'top'? halfStroke * signY: 0);
+      var borderBottom = bottom + (borderSkipped !== 'bottom'? -halfStroke * signY: 0);
+      // not become a vertical line?
+      if (borderLeft !== borderRight) {
+        top = borderTop;
+        bottom = borderBottom;
+      }
+      // not become a horizontal line?
+      if (borderTop !== borderBottom) {
+        left = borderLeft;
+        right = borderRight;
+      }
+    }
+
+    // calculate the bar width and roundess
+    var barWidth = Math.abs(left - right);
+    var roundness = this._chart.config.options.barRoundness || 0.5;
+    var radius = barWidth * roundness * 0.5;
+
+    // keep track of the original top of the bar
+    var prevTop = top;
+
+    // move the top down so there is room to draw the rounded top
+    top = prevTop + radius;
+    var barRadius = top - prevTop;
+
+    ctx.beginPath();
+    ctx.fillStyle = vm.backgroundColor;
+    ctx.strokeStyle = vm.borderColor;
+    ctx.lineWidth = borderWidth;
+
+    // draw the rounded top rectangle
+    Chart.helpers.drawRoundedTopRectangle(ctx, left, (top - barRadius + 1), barWidth, bottom - prevTop, barRadius);
+
+    ctx.fill();
+    if (borderWidth) {
+      ctx.stroke();
+    }
+
+    // restore the original top value so tooltips and scales still work
+    top = prevTop;
+  },
+});
+
+Chart.defaults.roundedBar = Chart.helpers.clone(Chart.defaults.bar);
+
+Chart.controllers.roundedBar = Chart.controllers.bar.extend({
+  dataElementType: Chart.elements.RoundedTopRectangle
+});
+
+
 $(function(){
 	// line carts
 	var chartPayout = document.getElementById("js-chart-payout"),
@@ -30,8 +137,9 @@ $(function(){
 		chartSummary3 = document.getElementById("js-chart-summary3"),
 
 		// used in performance.html page
-		chartSales = document.getElementById("js-chart-sales"),
-		chartCustomers = document.getElementById("js-chart-customers"),
+		chartSales = document.getElementById("js-chart-sales").getContext("2d");
+
+	var	chartCustomers = document.getElementById("js-chart-customers"),
 		chartProfit = document.getElementById("js-chart-profit");
 
 	var lineChartPayoutData = {
@@ -251,13 +359,16 @@ $(function(){
 	};
 
 	var barChartSalesData = {
-	   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-	    datasets: [{
-			label: "Profit",
+	    labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", 
+	    		 "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", 
+	    		 "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20"],
+
+		datasets: [{
+			label: "Sales",
 			fill: true,
 			backgroundColor: '#00AAFF',
-			data: [50, 60, 80, 40,50, 60]
-	    }]
+			data: [50, 60, 80, 40, 50, 60, 50, 60, 80, 40, 50, 60, 80, 40, 50, 60, 50, 60, 80, 40]
+		}]
 	};
 
 	var barChartCustomersData = {
@@ -826,11 +937,32 @@ $(function(){
 
 	if(chartSales) {
 		var barChartSales = new Chart(chartSales, {
-		    type: 'bar',
+		    type: 'roundedBar', 
 		    data: barChartSalesData,
 		    options: {
+		    	barRoundness: 1,
 		    	responsive: false,
-		    	maintainAspectRatio: false
+		    	maintainAspectRatio: false,
+		    	scales: {
+					xAxes: [{
+							display: false,
+							ticks: {
+							fontSize: '11',
+							fontColor: '#969da5'
+						},
+						gridLines: {
+							color: 'rgba(0,0,0,0.0)',
+							zeroLineColor: 'rgba(0,0,0,0.0)'
+						}
+					}],
+					yAxes: [{
+						display: false,
+						ticks: {
+							beginAtZero: true,
+							max: 100
+						}
+					}]
+				}
 			}
 		});
 	}
